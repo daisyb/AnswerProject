968232b7f380962b48de397f312a6537150a8f0c
Daisy <dbarbanel555@gmail.com>
2015-11-22 17:48:16 -0500

    added comments and funtions to read a url and find the most ocmmon value in a list

--- utils.py
+++ utils.py
@@ -1,10 +1,38 @@
-import google, urllib2, bs4, re
+from google import search
+from bs4 import BeautifulSoup
+from collections import Counter
+import urllib2
 
-def get_WebResults(q):
-    results = google.search(q,num=10,start=0,stop=10)
+def get_web_results(query):
+    """Retrieves list of 20 urls from google based on query.
 
+    Args:
+       query: A string containg a google query.
+
+    Returns:
+       A list of urls
+    """
+    results = search(query,num=20,start=0,stop=10)
     rlist = []
     for r in results:
         rlist.append(r)
-
     return rlist
+
+def read_page(url):
+    """Reads a url and returns parsed html.
+    
+    Args:
+        url: A string containg a valid url.
+
+    Returns:
+        A string containg the text of the webpage
+        referenced by url
+    """
+    req = urllib2.urlopen(url)
+    page = req.read()
+    soup = BeautifulSoup(page, 'html.parser')
+    return soup.get_text()
+
+def most_common_value(lst):
+    data = Counter(lst)
+    return data.most_common(1)[0][0]
