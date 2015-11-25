0d746339f3ffad703507981e68362301001c384f
tiffl <lei.tiffany@gmail.com>
2015-11-25 13:15:30 -0500

    basic webpage

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import utils, who, where, when
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
@@ -8,15 +9,19 @@ def home():
     else:
         question = request.form["question"]
         button = request.form["button"]
-        if button == "submit":
+        if button == "ask":
             webresults = utils.get_web_results(question)
-            if "who" in question:
-                answer = find_name(webresults)
-            else if "where" in question:
-                answer = find_place(webresults)
-            else if "when" in question:
-                answer = find_time(webresults)
+            if "who" in question.lower():
+                answer = who.find_name(webresults)
+            elif "where" in question.lower():
+                answer = where.find_place(webresults)
+            elif "when" in question.lower():
+                answer = when.find_time(webresults)
             else:
                 answer = "Question has to include who, where, or when."
             return render_template("results.html",ans=answer)
             
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0", port=8000)
+    
